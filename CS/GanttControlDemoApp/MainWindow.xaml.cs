b19c202636bab4b3e2c0b0f9211b174931bd5827
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-12-21 14:49:38 +0400

    Change example status to out of maintenance

--- CS/GanttControlDemoApp/MainWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace GanttControlDemoApp {
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
-}
